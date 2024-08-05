62af401a81713d9bccadd48e1623097b9ff6dc9b
Sergi Clara <sclar000@gmail.com>
2024-08-05 18:44:56 +0200

    XDP, GMC

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export PATH=$PATH:/mingw64/bin
+
+echo "Start building..."
 
 # create simple text file named 'host' in this folder with IP address of your MiSTer.
 
@@ -9,20 +12,19 @@ HOST=192.168.1.75
 # so we don't need to check the exit status of every command.
 set -e
 set -o pipefail
-
-echo "Start building..."
-make
-
+# make
+./compila.sh
 set +e
-echo y|plink root@$HOST -pw 1 'killall MiSTer'
-
+# echo y|plink root@$HOST -pw 1 'killall MiSTer'
 set -e
 ftp -n <<EOF
 open $HOST
 user root 1
 passive
 binary
-put MiSTer /media/fat/MiSTer
+put MiSTer_groovy_XDP /media/fat/MiSTer_groovy_XDP
+put MiSTer_groovy /media/fat/MiSTer_groovy
+#put poc /media/fat/poc
 EOF
 
-plink root@$HOST -pw 1 -batch 'sync;PATH=/media/fat:$PATH;MiSTer >/dev/ttyS0 2>/dev/ttyS0 </dev/null &'
+#plink root@$HOST -pw 1 -batch 'sync;PATH=/media/fat:$PATH;MiSTer >/dev/ttyS0 2>/dev/ttyS0 </dev/null &'
